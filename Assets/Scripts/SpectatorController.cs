ed6f9f6d757189134bd593713e3dd9d541b69ae0
Snorbius <joltii.lundqvist@gmail.com>
2026-01-14 15:44:17 +0100

    basic spectator controller

--- Assets/Scripts/SpectatorController.cs
+++ Assets/Scripts/SpectatorController.cs
@@ -4,15 +4,41 @@ using UnityEngine.InputSystem;
 public class SpectatorController : PlayerController
 {
     private Vector2 move;
+    private Vector2 look;
+
+    private Vector3 currentRot = Vector3.zero;
+    
     public override void PlayerUpdate()
     {
-        // camera.transform.position += Vector3.right * (Time.deltaTime * 2.0f);
-        // print("moving");
-        transform.Translate(move * Time.deltaTime * 2.0f);
+        camera.transform.position = transform.position;
+        camera.transform.forward = transform.forward;
+        
+        Vector3 translation = Vector3.zero;
+        translation += transform.forward * move.y;
+        translation += transform.right * move.x;
+        translation.Normalize();
+        
+        transform.Translate(translation * (10.0f * Time.deltaTime));
+
+        Vector3 rotation = Vector3.zero;
+        rotation += Vector3.up * look.x;
+        rotation += Vector3.right * -look.y;
+        rotation *= Time.deltaTime * 90.0f;
+
+        // Vector3 currentRot = transform.rotation.eulerAngles;
+        currentRot += rotation;
+        currentRot.x = Mathf.Clamp(currentRot.x, -88.0f, 88.0f);
+        transform.rotation = Quaternion.Euler(currentRot);
+        
     }
 
     private void OnMove(InputValue value)
     {
         move = value.Get<Vector2>();
+    }
+
+    private void OnLook(InputValue value)
+    {
+        look = value.Get<Vector2>();
     }
 }
