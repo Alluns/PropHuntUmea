173c1a09190672f03455cc8d6d633489814fd464
Alluns <allan.backman@gmail.com>
2026-01-14 15:16:22 +0100

    began working 2

--- Assets/Scripts/HiderController.cs
+++ Assets/Scripts/HiderController.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HiderController : PlayerController
 {
+    private Vector2 move;
+    private const float Speed = 3.0f;
+    
     public override void PlayerUpdate()
     {
+        transform.Translate(new Vector3(move.x, 0 ,move.y) * (Time.deltaTime * Speed));
+    }
+
+    private void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
     }
 }
