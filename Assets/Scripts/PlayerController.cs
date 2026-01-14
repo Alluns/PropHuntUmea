697c099a0416e765ce41b0f30fd164357529aa55
Noah Jörgensen <silvergrundnoah@gmail.com>
2026-01-14 16:11:35 +0100

    Started working on seeker logic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,25 @@
 using System;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public abstract class PlayerController : NetworkBehaviour
 {
     protected Camera camera;
+    public PlayerInput Input { get; private set; }
     
-    protected void Start()
-    {
-        camera = Camera.main;
+    protected virtual void Start()
+    {        
+        if (IsClient)
+            OnClientInit();
+        else
+
+
+            camera = Camera.main;
     }
+
+    protected virtual void OnClientInit() => Input = GetComponent<PlayerInput>();
+    protected virtual void OnHostInit() { }
 
     public abstract void PlayerUpdate();
 }
